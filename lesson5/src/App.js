7eeb557c2a7de44aec517d6fed6fbaa470c4c256
Anastasiia Polishchuk <polishchuck1818@gmail.com>
2024-03-19 17:48:51 +0200

    Update

--- lesson5/src/App.js
+++ lesson5/src/App.js
@@ -67,12 +67,36 @@
 
 // useEffect()
 
-import { useState, useEffect } from "react";
+import { useState, useRef, useEffect } from "react";
 
 function App() {
   const [mystate, setMystate] = useState(1);
   const [secondstate, setSecondstate] = useState(-100);
-  const [users, setUsers] = useState([]);
+  const [posts, setPosts] = useState([]);
+  const [comments, setComments] = useState([]);
+
+
+  const but1 = useRef();
+  // const but2 = useRef();
+  const inp1 = useRef([]);
+  const inp2 = useRef([]);
+  const inp3 = useRef([]);
+  const check1 = useRef();
+
+  function buttonListener(event) {
+    // console.log(event.target);
+    // console.log(but1.current);
+    // console.log(but2.current);
+    // console.log(but1.current.innerHTML);
+    // but1.current.style.backgroundColor = "red";
+    console.log(inp1.current.value);
+    console.log(inp2.current.value);
+    console.log(inp3.current.value);
+    // console.log(check1.current.checked);
+    // check1.current.checked = true;
+
+    // console.log(check1.current);
+  }
 
   function change() {
     setMystate(mystate + 10);
@@ -83,19 +107,32 @@ function App() {
   }
 
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
+    fetch('https://jsonplaceholder.typicode.com/posts')
       .then(response => response.json())
-      .then(json => setUsers(json))
+      .then(json => setPosts(json))
+  }, []);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/comments')
+      .then(response => response.json())
+      .then(json => setComments(json))
   }, []);
 
   return (
     <div className="App">
-      {/* {console.log("render component App")} */}
+          <input ref={inp1} type="text" />
+         <input ref={inp2} type="text" />
+      <input ref={inp3} type="text" />
+         <button ref={but1} onClick={buttonListener}>PUSH</button>
+      
       <p>My state is {mystate}</p>
       <button onClick={change}>CHANGE STATE</button>
       <button onClick={change}>CHANGE SECOND STATE</button>
-      <ul>
-        {users.map(e =><li key={e.id}>{e.name}</li>)}
+      <ul>posts:
+        {posts.map(e =><li key={e.id}>{e.title}</li>)}
+      </ul>
+      <ul>comments:
+        {comments.map(e => <li key={e.id}>{e.name}<p>{e.body}</p></li>)}
       </ul>
     </div>
   )
